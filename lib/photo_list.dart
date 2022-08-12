a5cbb84443debabb0c8f1d8ad1d893863aaa6404
masal9pse <returnymgstokh@icloud.com>
2022-08-12 22:01:31 +0900

    photo_listファイルを表示

---
+++ lib/photo_list.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PhotoList extends StatelessWidget {
+  const PhotoList({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('カメラテスト')),
+    );
+  }
+}
